2c830300d9f5ee28ba659eb369a26ca64acd7620
Zahar Litvinchuk <zahar@raynelz.ru>
2024-03-14 00:40:49 +0300

    intitialise base configuration of project

--- EventifyApp/Home/ViewController.swift
+++ EventifyApp/Home/ViewController.swift
@@ -6,14 +6,15 @@
 //
 
 import UIKit
+import SnapKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemGreen
     }
 
 
 }
-
