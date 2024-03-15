2159de8085430018eaad8c3458c3b6476fbd4755
Zahar Litvinchuk <zahar@raynelz.ru>
2024-03-15 14:16:40 +0300

    code style fix with swiftlint

--- EventifyApp/Home/ViewController.swift
+++ EventifyApp/Home/ViewController.swift
@@ -9,12 +9,8 @@ import UIKit
 import SnapKit
 
 final class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.backgroundColor = .systemGreen
     }
-
-
 }
