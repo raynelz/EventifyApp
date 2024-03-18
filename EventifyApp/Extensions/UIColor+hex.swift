3901822e43ddcdd146aa1bba03fd6beba12b7d7d
Zahar Litvinchuk <zahar@raynelz.ru>
2024-03-18 03:13:09 +0300

    fix code style

--- EventifyApp/Extensions/UIColor+hex.swift
+++ EventifyApp/Extensions/UIColor+hex.swift
@@ -20,13 +20,13 @@ extension UIColor {
         scanner.scanHexInt64(&color)
         
         let mask = 0x000000FF
-        let r = Int(color >> 16) & mask
-        let g = Int(color >> 8) & mask
-        let b = Int(color) & mask
-        
-        let red = CGFloat(r) / 255.0
-        let green = CGFloat(g) / 255.0
-        let blue = CGFloat(b) / 255.0
+        let redMask = Int(color >> 16) & mask
+        let greenMask = Int(color >> 8) & mask
+        let blueMask = Int(color) & mask
+
+        let red = CGFloat(redMask) / 255.0
+        let green = CGFloat(greenMask) / 255.0
+        let blue = CGFloat(blueMask) / 255.0
         
         self.init(red: red, green: green, blue: blue, alpha: 1)
     }
