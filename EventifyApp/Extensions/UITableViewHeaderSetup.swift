a8965718be280d2ab4947f5f954bd3852c55f341
Zahar Litvinchuk <zahar@raynelz.ru>
2024-05-04 13:20:24 +0300

    [fix] - constraint of headerView, add navigationBottomLine and color for it.Finally, new extension.

---
+++ EventifyApp/Extensions/UITableViewHeaderSetup.swift
@@ -0,0 +1,26 @@
+//
+//  UITableViewHeaderSetup.swift
+//  EventifyApp
+//
+//  Created by Захар Литвинчук on 04.05.2024.
+//
+
+import UIKit
+
+extension UITableView {
+    func setup(header: UIView) {
+        header.translatesAutoresizingMaskIntoConstraints = false
+
+        tableHeaderView = header
+
+        NSLayoutConstraint.activate([
+            header.widthAnchor.constraint(equalTo: widthAnchor),
+            header.topAnchor.constraint(equalTo: topAnchor)
+        ])
+    }
+
+    func updateHeaderLayout() {
+        guard let tableHeader = tableHeaderView else { return }
+        tableHeader.layoutIfNeeded()
+    }
+}
